f42c492ec5904f5dc8c4e881663b02ac8c513420
Vikrant Ahiwale <vikrant.ahiwale@speridian.com>
2026-06-12 17:25:00 +0530

    Fix:image visibility cards

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === "true";
+const isGithubPages = process.env.GITHUB_PAGES === "true" || process.env.GITHUB_ACTIONS === "true";
 const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "VJ-car-rental";
 const basePath = isGithubPages ? `/${repoName}` : "";
 
 const nextConfig = {
   images: {
     unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 
